769732526950f0d5143156cdd2cb6074bdb80fdb
vaperLee <rize1126jesse@gmail.com>
2024-05-02 17:29:20 +0900

    print the maze

--- path_finder.py
+++ path_finder.py
@@ -3,9 +3,35 @@ from curses import wrapper
 import queue
 import time
 
+maze = [
+    ["#", "#", "#", "#", "#", "#", "#", "O", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", "#", "#"],
+    ["#", " ", "#", " ", " ", " ", " ", " ", "#"],
+    ["#", "#", "#", "X", "#", "#", "#", "#", "#"]
+]
+
+def print_maze(maze, stdscr, path=[]):
+    RED = curses.color_pair(1)
+    BLUE = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            stdscr.addstr(i, j*2, value, RED)
+
+
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    blue_and_black = curses.color_pair(1)
+
+
     stdscr.clear()
-    stdscr.addstr(0, 0, "Hello world")
+    print_maze(maze, stdscr)
     stdscr.refresh()
     stdscr.getch()
 
