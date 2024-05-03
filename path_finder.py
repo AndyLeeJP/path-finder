0ddc6d9213dc5f0e3253a6fd5af59b90cb27f8d0
vaperLee <rize1126jesse@gmail.com>
2024-05-03 22:44:41 +0900

    implement class for using when look for start position at first

--- path_finder.py
+++ path_finder.py
@@ -23,12 +23,25 @@ def print_maze(maze, stdscr, path=[]):
         for j, value in enumerate(row):
             stdscr.addstr(i, j*2, value, RED)
 
+def start_position(maze, start):
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if value == start:
+                return i, j
+
+
+def find_path(maze, stdscr):
+    start = "o"
+    end = "x"
+    start_place = start_position(maze, start)
+
+    q = queue.Queue()
+    q.put((start_place, [start_place]))
+
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
-    blue_and_black = curses.color_pair(1)
-
 
     stdscr.clear()
     print_maze(maze, stdscr)
